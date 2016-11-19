3b80092e9a53df5e88b938ec12a558834b7e21eb
liuwei <liuwei@yinnut.com>
2016-11-19 21:50:45 +0800

    利用朴素贝叶斯过滤垃圾邮件

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -1,5 +1,6 @@
 # -*- encoding:utf-8 -*-
 from numpy import zeros, ones, log, array
+import random
 
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -68,7 +69,57 @@ def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     else:
         return 0
 
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
 
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok)>2]
+
+#first need email in directionay
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):
+        #垃圾邮件
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        # 非垃圾邮件
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet = []
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print "the error rate is: ", float(errorCount)/len(testSet)
+    
 if __name__ == '__main__':
     listOPosts, listClasses = loadDataSet()
     myVocabList = createVocabList(listOPosts)
