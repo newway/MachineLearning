d8d76303c631c7b547a98e9d1a9c1e5bcde81358
liuwei <liuwei@yinnut.com>
2016-12-24 16:53:49 +0800

    岭回归，前向逐步线性回归
    有函数未找到对应模块导入

--- regression/regression.py
+++ regression/regression.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 from numpy import *
+from math import *
+from sklearn import *
 
 def loadDataSet(fileName):
     numFeat = len(open(fileName).readline().split('\t')) - 1
@@ -50,5 +52,63 @@ def lwlrTest(testArr, xArr, yArr, k=1.0):
     for i in range(m):
         yHat[i] = lwlr(testArr[i], xArr, yArr, k)
     return yHat
+
+#岭回归，为防止X.T*X 是奇异矩阵，加上一个lam*I,   w^ = (X.T * X + lam*I).I * X.T * y
+def ridgeRegres(xMat, yMat, lam=0.2):
+    xTx = xMat.T * xMat
+    denom = xTx + eye(shape(xMat)[1]) * lam
+    if linalg.det(denom) == 0.0:
+        print "this matrix is singular, cannot do inverse"
+        return
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat, 0)
+    xVar = var(xMat, 0)
+    xMat = (xMat - xMeans) / xVar
+    numTestPts = 30
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, exp(i-10))
+        wMat[i,:] = ws.T
+    return wMat
+
+#前向逐步线性回归
+def stageWise(xArr, yArr, eps=0.01, numIt=100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMeans = mean(xMat, 0)
+    #均方差，与平均值的平均距离
+    xVar = var(xMat, 0)
+    #xMat = regularize(xMat)
+    xMat = (xMat - xMeans) / xVar
+    m,n = shape(xMat)
+    returnMat = zeros((numIt, 1))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+
+        ws = wsMax.copy()
+        returnMat[i,:] = ws.T
+    return returnMat
 
 
