0524101d61963d703f2e029d9704b4b0b17c19e1
liuwei <liuwei345@gmail.com>
2017-02-12 16:19:48 +0800

    apriori算法进行关联规则分析，P生成H的可信度计算，关联规则的生成与合并

--- Apriori/apriori.py
+++ Apriori/apriori.py
@@ -62,3 +62,37 @@ def apriori(dataSet, minSupport=0.5):
         L.append(Lk)
         k += 1
     return L, supportData
+
+#关联规则生成函数
+def generateRules(L, supportData, minConf=0.7):
+    bigRuleList = []
+    #从包含2个及以上元素的集合开始
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            if i > 1:
+                rulesFromConseq(freqSet, H1, supportData, bigRuleList, minConf)
+            else:
+                calcConf(freqSet, H1, supportData, bigRuleList)
+    return bigRuleList
+
+#计算规则可信度，一条规则P -》 H的可信度定义为：support(P | H) / support(P)
+def calcConf(freqSet, H, supportData, brl, minConf=0.7):
+    prunedH = []
+    for conseq in H:
+        conf = supportData[freqSet] / supportData[freqSet-conseq]
+        if conf >= minConf:
+            print freqSet-conseq, '-->', conseq, 'conf:',conf
+            brl.append((freqSet-conseq, conseq, conf))
+            prunedH.append(conseq)
+    return prunedH
+
+#生成候选规则集合
+def rulesFromConseq(freqSet, H, supportData, brl, minConf=0.7):
+    #m元项，H中每个元素是一个包含m个项的list
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = aprioriGen(H, m+1)
+        Hmp1 = calcConf(freqSet, Hmp1, supportData, brl, minConf)
+        if len(Hmp1) > 1:
+            rulesFromConseq(freqSet, Hmp1, supportData, brl, minConf)
