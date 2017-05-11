ba6deb4a8262f52d5fe4a22449fa538a88eb4ca4
liuwei <liuwei345@gmail.com>
2017-05-11 21:24:49 +0800

    增加说明

--- PCA/pca.py
+++ PCA/pca.py
@@ -12,10 +12,12 @@ def pca(dataMat, topNfeat=9999999):
     #去平均值
     meanVals = mean(dataMat, axis=0)
     meanRemoved = dataMat - meanVals
-    #计算协方差
+    #计算协方差，协方差即为空间变换矩阵
     covMat = cov(meanRemoved, rowvar=0)
     #方阵的 特征值 和 特征向量
     eigVals, eigVects = linalg.eig(mat(covMat))
+    print "val: ", eigVals
+    print "vect: ", eigVects
     eigValInd = argsort(eigVals)
     eigValInd = eigValInd[:-(topNfeat+1):-1]
     redEigVects = eigVects[:,eigValInd]
@@ -23,3 +25,9 @@ def pca(dataMat, topNfeat=9999999):
     lowDDataMat = meanRemoved * redEigVects
     reconMat = (lowDDataMat * redEigVects.T) + meanVals
     return lowDDataMat, reconMat
+
+def x():
+    a = mat([[1,2,3],[2,3,4],[3,4,5],[4,5,6],[5,6,6]])
+    pca(a)
+
+x()
